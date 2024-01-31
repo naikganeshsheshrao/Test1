30202212a2edb3d58e8590a28db8e17e3da152c2
Your Name <you@example.com>
2024-01-31 10:53:12 +0530

    added arraylist number for code

--- GitDemo/src/com/velocity/ArrayListDemo1.java
+++ GitDemo/src/com/velocity/ArrayListDemo1.java
@@ -1,5 +1,15 @@
 package com.velocity;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo1 {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		  al.add("pune");
+		  al.add("mumbai");
+		  al.add("delhi");
+		  al.add("Hyderabad");
+		  System.out.println(al);
+	}
 
 }
